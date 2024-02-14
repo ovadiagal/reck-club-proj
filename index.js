ee3726991d20fd11256dcb9d5f54f6776b71a960
Ovadia, Gal <gal.ovadia@gatech.edu>
2024-02-13 19:07:52 -0500

    basic endpoints

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/webhook', (req, res) => {
+  // Handle the webhook request here
 });
 
 app.listen(port, () => {
